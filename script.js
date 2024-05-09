ae6165e5f8a8ecf159baf8c7f81b1a8150d381b0
esot0001 <esot0001@algonquinlive.com>
2024-05-09 01:51:48 -0400

    Refactored increase counter button, prevented counter from going below 0, and handled sync button click

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // cleaner code to have all selectors in one place
-const increaseButtonEl = document.querySelector('.counter__button--increase');
 const counterValueEl = document.querySelector('.counter__value');
+const increaseButtonEl = document.querySelector('.counter__button--increase');
 const decreaseButtonEl = document.querySelector('.counter__button--decrease');
+const resetButtonEl = document.querySelector('.counter__reset-button');
 
-increaseButtonEl.addEventListener('click', function(){
+function incrementCounterValue(){
     //get current value of counter as string
     const currentCounterValue = counterValueEl.textContent;
 
@@ -15,8 +16,10 @@ increaseButtonEl.addEventListener('click', function(){
 
     //set and display new counter value
     counterValueEl.textContent = increasedCounterValue;
-    
-});
+}
+
+increaseButtonEl.addEventListener('click', incrementCounterValue);
+document.addEventListener('keydown', incrementCounterValue);
 
 decreaseButtonEl.addEventListener('click', function(){
     //get current value of counter as string
@@ -26,10 +29,21 @@ decreaseButtonEl.addEventListener('click', function(){
     const currentCounterValueToNumber = +currentCounterValue;
 
     // decrease counter value by 1++ for every click
-    increasedCounterValue = currentCounterValueToNumber - 1;
+    //I declared this variable without a keyword, what are the repercussions? Is this advisable or should it be actively avoided?
+    decreasedCounterValue = currentCounterValueToNumber - 1;
+
+    //prevent value from being less than 0
+    if(decreasedCounterValue <0){
+        decreasedCounterValue =0;
+    }
 
     //set and display new counter value
-    counterValueEl.textContent = increasedCounterValue;
-})
+    counterValueEl.textContent = decreasedCounterValue;
+});
+
+resetButtonEl.addEventListener("click", function(){
+    //set current value back to 0
+    counterValueEl.textContent = 0;
+});
 
 
